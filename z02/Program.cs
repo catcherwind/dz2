b787d40023e17bdaf55936e804d6188e789a8b52
Ksusha <ksushalive@yandex.ru>
2022-11-24 20:23:55 +0300

    init: solution of 2 task

---
+++ z02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+string n = Console.ReadLine();
+int n_col = int.Parse(n);
+if (n_col >= 100)
+    Console.WriteLine($"Третья цифра числа - {n[2]}");
+else if (n_col <= -100)
+    Console.WriteLine($"Третья цифра числа - {n[3]}");
+else
+    Console.WriteLine("Число слишком короткое, третьей цифры нет");
